8b2c78a7e06a15948021e0b82797c1237d4907f0
dengyong <theqtkc@gmail.com>
2019-08-16 13:15:17 +0800

    bugfix

--- page/xorm/x.go
+++ page/xorm/x.go
@@ -6,7 +6,6 @@ import (
 	"github.com/no-bibi/bugs/page"
 	"github.com/no-bibi/bugs/page/options"
 	"math"
-	"reflect"
 	"strings"
 )
 
@@ -61,10 +60,10 @@ func (this *Xorm) Page(data interface{}) page.Result {
 	}
 
 	//always make sure data is [] not null
-	if reflect.ValueOf(data).Elem().Len() == 0 {
-		original := reflect.MakeSlice(reflect.ValueOf(data).Elem().Type(), 0, 0).Interface()
-		data = original
-	}
+	//if reflect.ValueOf(data).Elem().Len() == 0 {
+	//	original := reflect.MakeSlice(reflect.ValueOf(data).Elem().Type(), 0, 0).Interface()
+	//	data = &original
+	//}
 
 	return page.Result{
 		Count:       int(count),
