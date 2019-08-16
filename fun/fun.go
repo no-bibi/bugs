e03706dfa0c0a88592758290dd75d2f81e585af0
dengyong <theqtkc@gmail.com>
2019-08-16 13:46:23 +0800

    bugfix

--- fun/fun.go
+++ fun/fun.go
@@ -105,9 +105,8 @@ func MakeClone(data interface{}) interface{} {
 
 	if reflect.ValueOf(data).Elem().Len() == 0 {
 		t := reflect.ValueOf(data).Elem().Type()
-		arr := reflect.MakeSlice(t, 0, 0)
 		data := reflect.New(reflect.ValueOf(data).Elem().Type())
-		data.Elem().Set(arr)
+		data.Elem().Set(reflect.MakeSlice(t, 0, 0))
 		return data.Interface()
 	}
 	return data
